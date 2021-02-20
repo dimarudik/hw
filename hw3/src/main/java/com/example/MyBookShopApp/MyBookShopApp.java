39df5ecbadcd89908222a44bb643f4c2bc2b34b0
Dmitrii Rudik <dima.rudik@gmail.com>
2021-02-20 08:55:28 +0300

    DOCS added

---
+++ hw3/src/main/java/com/example/MyBookShopApp/MyBookShopApp.java
@@ -0,0 +1,13 @@
+package com.example.MyBookShopApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBookShopApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBookShopApp.class, args);
+	}
+
+}
---
+++ hw3/src/main/java/com/example/MyBookShopApp/controllers/RedirectController.java
@@ -0,0 +1,31 @@
+package com.example.MyBookShopApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class RedirectController {
+
+    @GetMapping("/index.html")
+    public ModelAndView redirectWithUsingForwardPrefixMain(ModelMap model) {
+        model.addAttribute("attribute", "forwardWithForwardPrefixMain");
+        return new ModelAndView("forward:/bookshop/main", model);
+    }
+
+    @GetMapping("/genres/index.html")
+    public ModelAndView redirectWithUsingForwardPrefixGenres(ModelMap model) {
+        model.addAttribute("attribute", "forwardWithForwardPrefixGenres");
+        return new ModelAndView("forward:/bookshop/genres", model);
+    }
+
+    @GetMapping("/authors/index.html")
+    public ModelAndView redirectWithUsingForwardPrefixAuthors(ModelMap model) {
+        model.addAttribute("attribute", "forwardWithForwardPrefixAuthors");
+        return new ModelAndView("forward:/bookshop/authors", model);
+    }
+
+}
