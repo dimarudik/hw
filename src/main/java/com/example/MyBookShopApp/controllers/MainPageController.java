b10d362dffe1227924d4fdcf96def5103565c02d
Dmitrii Rudik <dima.rudik@gmail.com>
2021-04-15 12:04:23 +0300

    Issue with front?

---
+++ src/main/java/com/example/MyBookShopApp/controllers/MainPageController.java
@@ -0,0 +1,93 @@
+package com.example.MyBookShopApp.controllers;
+
+import com.example.MyBookShopApp.data.Book;
+import com.example.MyBookShopApp.data.BookService;
+import com.example.MyBookShopApp.data.BooksPageDto;
+import com.example.MyBookShopApp.data.SearchWordDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+@Controller
+public class MainPageController {
+
+    private Logger logger = Logger.getLogger(MainPageController.class.getSimpleName());
+
+    private final BookService bookService;
+
+    @Autowired
+    public MainPageController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @ModelAttribute("recommendedBooks")
+    public List<Book> recommendedBooks() {
+        return bookService.getPageOfRecommendedBooks(0, 6).getContent();
+    }
+
+    @ModelAttribute("searchWordDto")
+    public SearchWordDto searchWordDto() {
+        return new SearchWordDto();
+    }
+
+    @ModelAttribute("searchResults")
+    public List<Book> searchResults() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping("/")
+    public String mainPage() {
+        return "index";
+    }
+
+    @GetMapping("/books/recommended")
+    @ResponseBody
+    public BooksPageDto getBooksPage(@RequestParam("offset") Integer offset,
+                                     @RequestParam("limit") Integer limit) {
+        return new BooksPageDto(bookService.getPageOfRecommendedBooks(offset, limit).getContent());
+    }
+
+/*
+    @GetMapping("/books/recommended")
+    @ResponseBody
+    public BooksPageDto getBooksPage(@RequestParam("offset") Integer offset,
+                                     @RequestParam("limit") Integer limit) {
+        Page<Book> books = bookService.getPageOfRecommendedBooks(offset, limit);
+        BooksPageDto booksPageDto = new BooksPageDto(books.getContent());
+        //logger.info(books.toString());
+*/
+/*
+        booksPageDto
+                .getBooks()
+                .forEach(i -> {
+                    logger.info(i.getAuthor().getFirstName() + " " + i.getAuthor().getLastName());
+                });
+*//*
+
+        return booksPageDto;
+    }
+*/
+
+    @GetMapping(value = {"/search", "/search/{searchWord}"})
+    public String getSearchResults(@PathVariable(value = "searchWord", required = false) SearchWordDto searchWordDto,
+                                   Model model) {
+        model.addAttribute("searchWordDto", searchWordDto);
+        model.addAttribute("searchResults",
+                bookService.getPageOfSearchResultBooks(searchWordDto.getExample(), 0, 5).getContent());
+        return "/search/index";
+    }
+
+    @GetMapping("/search/page/{searchWord}")
+    @ResponseBody
+    public BooksPageDto getNextSearchPage(@RequestParam("offset") Integer offset,
+                                          @RequestParam("limit") Integer limit,
+                                          @PathVariable(value = "searchWord", required = false) SearchWordDto searchWordDto) {
+        return new BooksPageDto(bookService.getPageOfSearchResultBooks(searchWordDto.getExample(), offset, limit).getContent());
+    }
+}
